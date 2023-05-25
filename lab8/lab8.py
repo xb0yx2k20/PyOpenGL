411e31d88342096944b28da5c1fd7232386e2f02
xb0yx2k20 <musraul@yandex.ru>
2023-05-25 15:37:37 +0300

    lab8o

--- lab8/lab8.py
+++ lab8/lab8.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import math
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 #import sys
@@ -10,6 +11,30 @@ object_matrix = np.identity(4)
 
 last_mouse_x = 0
 last_mouse_y = 0
+n = 10
+wireframe_mode = False
+
+def handle_key_press(key, x, y):
+    global n, scale
+    if key == b'q':
+        n -= 1
+    if key == b'e':
+        n += 1
+    global wireframe_mode
+    if key == b'x':
+        wireframe_mode = not wireframe_mode
+        if wireframe_mode:
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+            glDisable(GL_CULL_FACE)
+        else:
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
+            glEnable(GL_CULL_FACE)
+    elif key == b'o':
+        scale -= 0.1
+    elif key == b'p':
+        scale += 0.1
+    glutPostRedisplay()
+
 
 def mouse_wheel_callback(wheel, direction, x, y):
     global scale
@@ -69,36 +94,73 @@ def create_shader(shader_type, source):
     glCompileShader(shader)
     return shader
 
-
+globalMas = []
+pointdata = []
 def draw():
+    global n, globalMas, scale, pointdata
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glEnableClientState(GL_VERTEX_ARRAY)            
     glEnableClientState(GL_COLOR_ARRAY)      
 
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
+    vectors = []
+    for i in range(n+1):
+        theta = 2 * math.pi * i / n 
+        for j in range(n+1):
+            phi = math.pi * j / n
+            x = math.cos(theta) * math.sin(phi)
+            y = math.sin(theta) * math.sin(phi)
+            z = math.cos(phi)
+            vectors.append([x, y, z])
+        globalMas.append(vectors)
+        vectors = []
+
+    for i in range(len(globalMas) - 1):
+        for j in range(len(globalMas[0]) - 1):
+            pointdata.append(globalMas[i + 1][j][0]) 
+            pointdata.append(globalMas[i + 1][j][1]) 
+            pointdata.append(globalMas[i + 1][j][2])
+            pointdata.append(globalMas[i][j][0]) 
+            pointdata.append(globalMas[i][j][1])
+            pointdata.append(globalMas[i][j][2])
+            pointdata.append(globalMas[i][j + 1][0]) 
+            pointdata.append(globalMas[i][j + 1][1]) 
+            pointdata.append(globalMas[i][j + 1][2])
+            pointdata.append(globalMas[i + 1][j + 1][0]) 
+            pointdata.append(globalMas[i + 1][j + 1][1]) 
+            pointdata.append(globalMas[i + 1][j + 1][2])
+
+    #globalMas = np.array(globalMas) 
+    #print((globalMas))
 
     glPushMatrix()
     glMultMatrixf(object_matrix)
 
     glScalef(scale*0.25, scale*0.25, scale*0.25)
+
     
+
     glVertexPointer(3, GL_FLOAT, 0, pointdata)
     glColorPointer(3, GL_FLOAT, 0, pointcolor)
-    glDrawArrays(GL_QUADS, 0, 24)
+    glDrawArrays(GL_QUADS, 0, 12 * n * n)
     glDisableClientState(GL_VERTEX_ARRAY)           
     glDisableClientState(GL_COLOR_ARRAY) 
     glPopMatrix()
     glutSwapBuffers()   
 
+
+
+
 glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB)
 glutInitWindowSize(800, 800)
 glutInitWindowPosition(50, 50)
-glutInit(sys.argv)
+glutInit()
 glutCreateWindow(b"Shaders!")
 glutReshapeFunc(lambda w, h: glViewport(0, 0, w, h))
 glEnable(GL_CULL_FACE)
 glutDisplayFunc(draw)
+print(globalMas)
 glutMouseFunc(handle_mouse_down)
 glutMotionFunc(handle_mouse_move)
 glutIdleFunc(draw)
@@ -120,18 +182,17 @@ glAttachShader(program, vertex)
 glAttachShader(program, fragment)
 glLinkProgram(program)
 glUseProgram(program)
+print(globalMas)
+print(globalMas)
+print(globalMas)
+print(globalMas)
 
-pointdata = [1, -1, 1, 1, 1, 1, -1, 1, 1, -1, -1, 1, 
-                      -1, -1, 1, -1, 1, 1, -1, 1, -1, -1, -1, -1, 
-                      -1, -1, -1, -1, 1, -1, 1, 1, -1, 1, -1, -1, 
-                      1, -1, -1, 1, 1, -1, 1, 1, 1, 1, -1, 1, 
-                      1, 1, 1, 1, 1, -1, -1, 1, -1, -1, 1, 1, 
-                      1, -1, 1, -1, -1, 1, -1, -1, -1, 1, -1, -1]
-pointcolor = [[1, 1, 0], [0, 1, 1], [1, 0, 1], [1, 1, 0],
-              [1, 1, 0], [0, 1, 1], [1, 0, 1], [1, 1, 0],
-              [1, 1, 0], [0, 1, 1], [1, 0, 1], [1, 1, 0],
-              [1, 1, 0], [0, 1, 1], [1, 0, 1], [1, 1, 0],
-              [1, 1, 0], [0, 1, 1], [1, 0, 1], [1, 1, 0],
-              [1, 1, 0], [0, 1, 1], [1, 0, 1], [1, 1, 0]]
+pointcolor = []
+print(pointdata)
+for i in range(12 * n * n):
+    pointcolor.append([1, i%2, 0])
+
+
+
 
 glutMainLoop()
